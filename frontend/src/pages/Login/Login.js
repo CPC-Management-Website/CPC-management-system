1eeebd8f670c2254ebe448f4de135cb8a7161a06
Mohamed Ayman <mohameddalash01@gmail.com>
2022-10-31 20:59:27 +0200

    modified frontend to include password field modified backend to implement login features added .env to store sensetive data

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -4,9 +4,10 @@ import APIService from '../../services/APIService';
 
 function Login(){
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const loginUser = () =>{
-        APIService.loginUser({email})
+        APIService.loginUser({email,password})
         .catch(error => console.log('error',error))
     }
 
@@ -15,14 +16,17 @@ function Login(){
         e.preventDefault();
         loginUser()
         setEmail('')
+        setPassword('')
     }
 
     return (
     <div className="auth-form-container">
         <h2>Login</h2>
         <form className="login-form" onSubmit={handleSubmit}>
-            <label htmlFor="email">email</label>
+            <label htmlFor="email">Email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">Password</label>
+            <input value={password} onChange={(e) => setPassword(e.target.value)}type="password" placeholder="**********" id="password" name="password" />
             <button type="submit">Log In</button>
         </form>
     </div>
