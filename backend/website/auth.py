da55005581f69db9cf5cb69c6f87168b15a524fe
Mohamed Ayman <mohameddalash01@gmail.com>
2022-11-14 21:18:10 +0200

    Merge branch 'master' of https://github.com/HanyaAdel/CPC-management-system

--- backend/website/auth.py
+++ backend/website/auth.py
@@ -1,8 +1,9 @@
 from flask import request, Blueprint
 from app import create_app
-from .models import User
+from .models import User, permissions
 from flask_login import current_user, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+import json
 
 auth = Blueprint("auth", __name__)
 
@@ -18,16 +19,21 @@ def get_data():
 
 @auth.route("/login", methods=["POST"], strict_slashes=False)
 def login():
-   
+
     email = request.json["email"]
     password = request.json["password"]
-
+    
     user = User.getUser(email)
 
     if user:
         if check_password_hash(user.password, password):
             print("Logged in!")
             login_user(user,remember = remember_logins)
+            perm = permissions(user)
+            user_json = json.dumps(user.__dict__)
+            permissions_json = json.dumps(perm.__dict__)
+            print(user_json)
+            print(permissions_json)
             # return redirect(url_for('views.home'))
         else:
             print("Password incorrect!")
@@ -35,4 +41,27 @@ def login():
     else:
         print("Email does not exist")
         return {"Error" : "Email does not exist"}, 402
+    # TODO what to return here
     return {"email" : email,"password" : password}
+
+@auth.route("/register", methods=["POST"], strict_slashes=False)
+def register():
+   
+    email = request.json["email"]
+    password = request.json["password"]
+    vjudge = request.json["vjudge"]
+    # email = input("email: ")
+    # password = input("password: ")
+    # vjudge = input("vjudge: ")
+    # TODO are the password checks done in the frontend or the backend
+    user = User.getUser(email)
+
+    if user:
+        print("user already registered")
+    else:
+        User.addUser(email, vjudge, password=generate_password_hash(
+                password, method='sha256'))
+        print("User added successfully")
+    # TODO what to return here?
+    return {"email" : email,"password" : password}
+# register()
--- backend/website/models.py
+++ backend/website/models.py
@@ -7,10 +7,11 @@ class User(UserMixin):
     name = None
     email = None
     level = None
-    type = None
+    role = None
     active = None
     points = None
     password = None
+    permissions = None
     
     def get_id(self):
         return self.id
@@ -19,10 +20,11 @@ class User(UserMixin):
         mycursor = db.cursor()
         query = "SELECT * FROM user where email=\""+email+"\""
         mycursor.execute(query)
-    def __init__(self, email,vjudge_handle,password):
+    def __init__(self, email,vjudge_handle,password,user_role):
         self.email = email
         self.password = password
         self.vjudge_handle = vjudge_handle
+        self.role = user_role
     
     def get_id(self):
         return self.email
@@ -42,17 +44,56 @@ class User(UserMixin):
     @staticmethod
     def addUser(email, vjudge_handle, password):
         mycursor = db.cursor()
-        query  = "INSERT INTO training.user (`email`, `vjudge_handle`,`type`,`password`) VALUES ('" +email+"','"+vjudge_handle+"','trainee','"+password+"');"
+        query  = "INSERT INTO training.user (`email`, `vjudge_handle`,`user_role`,`password`) VALUES ('" +email+"','"+vjudge_handle+"','trainee','"+password+"');"
         mycursor.execute(query)
         db.commit()
         print("here")
+        
     @staticmethod
     def getUser(email):
         mycursor = db.cursor()
-        query  = "SELECT `email` ,`vjudge_handle`, `password` from user where email = \""+email+"\""
+        query  = "SELECT `email` ,`vjudge_handle`, `password`, `user_role` from user where email = \""+email+"\""
         mycursor.execute(query)
         user = None
         for x in mycursor:
-            user = User(x[0],x[1],x[2])
+            user = User(x[0],x[1],x[2],x[3])
         return user
     
+    @staticmethod
+    def getPermissions(user):
+        def getRoleID(role):
+            #TODO sql query to get role id from role name
+            return 1
+        ID = getRoleID(User.role)
+        #TODO write sql for getting role permissions
+        return permissions()
+    
+class permissions():
+    read_weekly_status = False
+    read_task =  False
+    create_task = False
+    update_task = False
+    read_resource = False
+    create_resource = False
+    read_trainees = False
+    create_trainees = False
+    update_trainees = False
+    delete_trainees = False
+    read_transcript = False
+    
+    @staticmethod
+    def getRoleID(role):
+        mycursor = db.cursor()
+        stmt = "SELECT role_id from role where user_role = %s;"
+        mycursor.execute(stmt,(role,))
+        ID =  mycursor.fetchone()[0]
+        return ID
+
+    def __init__(self,user:User):
+        ID = permissions.getRoleID(user.role)
+        mycursor = db.cursor()
+        stmt = "SELECT * from permission where role_id = %s;"
+        mycursor.execute(stmt,(ID,))
+        temp = dict(zip(mycursor.column_names, mycursor.fetchone()))
+        for key in temp:
+            setattr(self,key,temp[key])
