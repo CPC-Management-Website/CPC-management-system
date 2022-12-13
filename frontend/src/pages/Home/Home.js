e6231f505f6d803663adbbf41dba37459cdd7903
SaraHussein28 <sara.hussein286@gmail.com>
2022-12-13 19:53:40 +0200

    added some styling to the landing page.

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,15 +1,20 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import { ReactComponent as Logo } from './developer.svg';
 import "./Home.css" 
 function Home(){
     return (
     <div>
         <NavBar/>
 
-        <div className="WelcomeText-container">
+        <div className="welcome-page-horizontal-container" >
+        <div className="welcome-page-vertical-container" >
         <label className="WelcomeLabel">Welcome Back! </label>
+        <label className="welcome-label-description"> Create resources, manage enrollment, track the performance of each trainee and build learning community — all in one place</label>
         </div>
+        <Logo width = "600" height = "600"/>
 
+        </div>
     </div>
     );
 }
