231c6813494b59da861ec793eeed55c814a69037
sama85 <samayasser8520@gmail.com>
2022-12-21 19:02:56 +0200

    modified profile editing

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
-from flask import Blueprint, request
+from flask import request
 from Vjudge_api import get_vjudge_data
 from .models import ProgressPerContest
 from . import db
 from .__init__ import urls
+from models import User
 
 views = Blueprint("views", __name__)
 
@@ -22,3 +23,30 @@ def get_progress_per_contest():
         print (x)
 
     return res
+
+
+#this method shall be called by email?
+#@views.route('/profile/edit/<email>', methods = ["POST, GET"])
+#@views.route(urls.PROFILE, methods = ["POST, GET"], strict_slashes=False)
+def editProfile(email):
+    if request.method == "POST":
+
+        #if post, won't update email?
+        #email = request.json["email"]
+        vjudge_handle = request.json["vjudge_handle"]
+        name = request.json["name"]
+        password = request.json["password"]
+
+        User.changePasswordAdmin(email, password)
+        mycursor = db.cursor()
+        query = "UPDATE user SET vjudge_handle=%s, name=%s WHERE email=%s"
+        mycursor.execute(query, (vjudge_handle, name, email))
+        db.commit()
+        #how to redirect to profile after update?
+        #return redirect ('/profile')
+
+    mycursor = db.cursor()
+    user = User(email)
+
+    #return user object to be displayed?
+    return user
--- frontend/src/backend_urls.js
+++ frontend/src/backend_urls.js
@@ -3,5 +3,5 @@ const DATA = "/data"
 const LOGIN = "/login"
 const USER_ENTRY_FILE = '/userentryfile'
 const PROGRESS_PER_CONTEST = '/progresspercontest'
-
+const PROFILE = '/profile'
 //export {PROGRESS_PER_CONTEST, USER_ENTRY_FILE, LOGIN, DATA, USER_ENTRY}
