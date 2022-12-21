62fbd629eddfb341323e258fd869e1cec48a1db5
HanyaAdel <hanya.elged@gmail.com>
2022-12-21 17:41:32 +0200

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -19,8 +18,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
