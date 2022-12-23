dc18e9eb398a9cfec323c5d2c7906e4d717408ba
Basmala-Abdullah <96146987+Basmala-Abdullah@users.noreply.github.com>
2022-12-23 16:59:29 +0200

    Updated Profile.js

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -38,8 +38,9 @@ function Profile() {
       <div>
       <NavBar/>
 
-          <div className="auth-form-container">
-              <h2>Edit your profile</h2>
+          <div md="4" className="gradient-custom auth-form-container">
+          <h1><img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp" alt="Avatar" className="my-5" style={{alignItems: 'center', width: '300px' }} fluid /></h1>
+              
               <form className="userEntry-form" onSubmit={handleSubmit}>
                   
                   <label htmlFor="Name">Your Name</label>
@@ -60,68 +61,13 @@ function Profile() {
                   <input value={password} onChange={(e) => setPassword(e.target.value)}type="password" placeholder="*****" id="password" name="password" disabled = {!edit}/>
 
                  {edit ? (
-                 <button type="submit">Save</button>
+                 <h1><button style={{color: 'salmon'}} type="submit">Save</button></h1>
                   ) : (
-                    <button onClick={(e) => editUser(e)}>Edit</button>
+                    <h1><button style={{color: 'salmon'}} onClick={(e) => editUser(e)}>Edit</button></h1>
                    )}
-              </form>
-
-
-
-            
+              </form>            
           </div>
-
       </div>
-
   );
-
-
-//   return (
-// <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>
-//       <MDBContainer className="py-5 h-100">
-//         <MDBRow className="justify-content-center align-items-center h-100">
-//           <MDBCol lg="6" className="mb-4 mb-lg-0">
-//             <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
-//               <MDBRow className="g-0">
-//                 <MDBCol md="4" className="gradient-custom text-center text-white"
-//                   style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
-//                   <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-//                     alt="Avatar" className="my-5" style={{ width: '300px' }} fluid />
-//                   <MDBTypography tag="h1">User Name</MDBTypography>
-
-//                   <MDBIcon far icon="edit mb-5" />
-//                 </MDBCol>
-//                 <MDBCol md="8">
-//                   <MDBCardBody className="p-4">
-
-                    
-//                     <MDBRow className="pt-1">
-//                       <MDBCol size="6" className="mb-3">
-//                         <MDBTypography tag="h6">Email</MDBTypography>
-//                         <MDBCardText className="text-muted">user@gmail.com</MDBCardText>
-//                       </MDBCol>
-//                       <MDBCol size="6" className="mb-3">
-//                         <MDBTypography tag="h6">Vjudge Handle</MDBTypography>
-//                         <MDBCardText className="text-muted">Vjudge Handle</MDBCardText>
-//                       </MDBCol>
-//                       <MDBCol size="6" className="mb-3">
-//                         <MDBTypography tag="h6">ID</MDBTypography>
-//                         <MDBCardText className="text-muted">12345</MDBCardText>
-//                       </MDBCol>
-//                       <MDBCol size="6" className="mb-3">
-//                         <MDBTypography tag="h6">Password</MDBTypography>
-//                         <MDBCardText className="text-muted">*********</MDBCardText>
-//                       </MDBCol>
-                      
-//                     </MDBRow>
-//                   </MDBCardBody>
-//                 </MDBCol>
-//               </MDBRow>
-//             </MDBCard>
-//           </MDBCol>
-//         </MDBRow>
-//       </MDBContainer>
-//     </section>
-//   );
-              }
+}
 export default Profile;
