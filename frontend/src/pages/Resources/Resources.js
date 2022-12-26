9ec7aed1fb0fcd328b8b0f9b5243270e0e0ef4e8
SaraHussein28 <sara.hussein286@gmail.com>
2022-12-26 16:35:08 +0200

    Refactored Rsources Page

--- frontend/src/pages/Resources/Resources.js
+++ frontend/src/pages/Resources/Resources.js
@@ -1,99 +1,79 @@
-import React, {Component} from 'react' 
+import React, {Component, useState, useEffect} from 'react' 
 import AddResource from '../components/AddResource'
 import ResourcesList from '../components/ResourcesList'
 import './Resources.css';
 import NavBar from "../NavBar/NavBar";
 
-class Resources extends Component {
-
-  
-  constructor() {
-    super();
-    this.state = JSON.parse(window.localStorage.getItem('state')) || {
-    resources:[
+function Resources ()
+// extends Component 
+{
+    const initialResources = [
       {name: "Week #1 Session: "},
       {name: "Week #2 Session: "},
-      {name: "Week #3 Session: "}
-    ],
-    }
-  }
-  setState(state) {
-    window.localStorage.setItem('state', JSON.stringify(state));
-    super.setState(state);
-  }
+     {name: "Week #3 Session: "}
+    ];
 
-  // state={
-  //   resources:[
-  //     {name: "Week #1 Session: "},
-  //     {name: "Week #2 Session: "},
-  //     {name: "Week #3 Session: "}
-  //   ],
-  //   current: ''
-  // }
-
+    
+    //States
+    const [Resources, setResources] = useState ([]);
+    //const [Resources, setResources] = useState(initialResources);
+   
+    const [current, setCurrent] =  useState("");
+  
   //updateResource (track the new typed resource)
-  updateResource = (e) =>{
+  const updateResource = (e) =>{
     e.preventDefault();
-    this.setState({
-        current: e.target.value //input value
-      })
+
+    setCurrent({current: e.target.value }) //input value
   }
+
 
   //AddNewResource
-  addNewResource = (e) => {
+  const addNewResource = (e) => {
     e.preventDefault();
-   let crr = this.state.current;
-   let sk = this.state.resources;
-   sk.push({name:crr})
-   this.setState({
-    resources: sk,
-    current: ''
-   })
-  }
+  let crr = current;
+  setResources([
+       ...Resources, // that contains all the old items
+          {name: crr}
+         ]); 
+   setCurrent ("");  
+}
 
   //deleteResource
-
-  deleteResource = (i) => {
-    let skls = this.state.resources;
+const deleteResource = (i) => {
+    let skls = Resources;
     skls.splice(i, 1); //remove
-    this.setState({
-      resources: skls
-    })
+    setResources(skls);
   }
 
   //editResource
-  editResource = (i, newval) =>{
-    let sklls = this.state.resources;
+const editResource = (i, newval) =>{
+    let sklls = Resources;
+
     let skl = sklls[i];
     skl['name']= newval;
-    this.setState({
-      resources: sklls
-    })
+    setResources(skl)
   }
 
-
-  render(){
-    const resourcesList = this.state.resources.map((resource, i) => {
-      return <ResourcesList x={resource} key={i} index={i} del={this.deleteResource} editt={this.editResource}/>
-    })
-    
     return (
       <div>
       <NavBar/>
       <div className="Resources">
         <h2 className="heading"><span className="fas fa-bahai awsome"></span> Manage Your Resources</h2>
-        <AddResource y={this.updateResource} z={this.addNewResource} cr={this.state.current}/>
+        <form onSubmit={addNewResource}>
+                <input type="text" placeholder="Add new resource"  value={current} onChange={(e) => setCurrent(e.target.value)} className="inputresource" />
+                <button className="btn btnresource">Add Resource</button>
+            </form>
+            
         <div className="current">
           <p className="currentp">Your Current Resources: </p>
-          <ul className="resourceList">{resourcesList}</ul>
+          <ul className="resourceList">{Resources.map((resource, i) => (
+        <ResourcesList x={resource} key={i} index={i} del={deleteResource} editt={editResource}/>))}</ul>
+
         </div>
         <h3 className="heading3">ASUFE CPC</h3>
-
       </div>
       </div>
 
     );
-  }
-}
-
-export default Resources;
+  }export default Resources;
