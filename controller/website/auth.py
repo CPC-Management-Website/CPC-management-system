2e2fe9fa603e32e9a3745d464a7b4c420ac5394a
Mohamed Ayman <mohameddallash21@gmail.com>
2023-02-01 18:42:03 +0200

    bug fix

--- controller/website/auth.py
+++ controller/website/auth.py
@@ -54,7 +54,7 @@ def register():
     else:
         User.addUser(vjudge_handle = vjudge,name = name,
                     email = email, level = level,roleID = roleID,
-                    active = True, points = 0,
+                    enrolled = True, points = 0,
                     password = generate_password_hash(password, method='sha256'))
         print("User added successfully")
         sendPasswordEmails([{"name":name,"password":password,"email":email}])
@@ -90,7 +90,7 @@ def registerfile():
         else:
             User.addUser(vjudge_handle = vjudge,name = name,
                     email = email, level = level,roleID = roleID,
-                    active = True, points = 0,
+                    enrolled = True, points = 0,
                     password = generate_password_hash(password, method='sha256'))
             print(email, " added successfully")
             emails.append({"email":email,"name":name,"password":password})
